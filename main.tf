334135c5a050d96a68ee9245c4eb0391ef63b203
KaoriAkagi <149767729+KaoriAkagi@users.noreply.github.com>
2025-06-19 07:06:40 +0900

    Deploy a webserver

--- main.tf
+++ main.tf
@@ -6,9 +6,29 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami = "ami-0bb2c57f7cfafb1cb"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
+  user_data = <<-EOF
+	      #!/bin/bash
+              echo "Hello, World" > index.html
+	      nohup python3 -m http.server 8080 &
+	      EOF
+
+  user_data_replace_on_change = true
 
   tags = {
     Name = "terraform-example"
+  }
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
